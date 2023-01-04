0ed48aa1ae359299d0f30cabb3f5822f65b1d104
Fit4a <67219161+nothome2005@users.noreply.github.com>
2023-01-04 23:06:33 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,2 +1,12 @@
-var snd = new Audio("music/My-Feelings-Doki-Doki-Literature-Club.wav");
+const snd = new Audio("music/My-Feelings-Doki-Doki-Literature-Club.wav");
+const btn = document.getElementById('btn');
+const Label = document.getElementById('LABEL')
 snd.play();
+const dialogs = ['Привет!', 'Я наконец-то заработала!','Я так рада этому!']
+var num = 0;
+
+// ✅ Change button text on click
+btn.addEventListener('click', function handleClick() {
+  Label.textContent = dialogs[num];
+  num++;
+});
