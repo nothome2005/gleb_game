121128ca7039695926c96ebc4553ab217196889f
Fit4a <67219161+nothome2005@users.noreply.github.com>
2023-01-05 22:16:07 +0200

    Update script.js

--- script.js
+++ script.js
@@ -9,26 +9,31 @@ var num = 0;
 var anim_var = 0;
 var speed = 50;
 
-function Anim_Text() {
-  if (anim_var < (dialogs[num].length + 1)) {
-    Label.textContent = dialogs[num].slice(0,anim_var);
-    anim_var++;
-    setTimeout(Anim_Text, speed);
-  } else {
-    anim_var = 0;
-    num++;
-  }
-}
-
-
-btn.addEventListener('click', function handleClick() {
+function change_dialog_logo() {
     if (dialog_img[num] == 'man') {
         character.src = 'dialog window/DialogWindow-H(2).png';
     }
     if (dialog_img[num] == 'girl') {
         character.src = 'dialog window/DialogWindow-Komi(2).png'
     }
-    
+}
+
+function Anim_Text() {
+
+    change_dialog_logo()
+    if (anim_var < (dialogs[num].length + 1)) {
+        Label.textContent = dialogs[num].slice(0,anim_var);
+        anim_var++;
+        setTimeout(Anim_Text, speed);
+    } else {
+        anim_var = 0;
+        num++;
+  }
+}
+
+
+btn.addEventListener('click', function handleClick() {
+    change_dialog_logo()
     if (num == (dialogs.length))
     {
         num = 0;
