cba9f4df4abf241c60a9bb60eb96f80039db905e
Fit4a <67219161+nothome2005@users.noreply.github.com>
2023-01-05 19:29:18 +0200

    Update script.js

--- script.js
+++ script.js
@@ -4,13 +4,27 @@ const Label = document.getElementById('LABEL')
 snd.play();
 const dialogs = ['Привет!', 'Я наконец-то заработала!','Я так рада этому!']
 var num = 0;
+var anim_var = 0;
+var speed = 50;
 
+function Anim_Text() {
+  if (anim_var < (dialogs[num].length + 1)) {
+    Label.textContent = dialogs[num].slice(0,anim_var);
+    anim_var++;
+    setTimeout(Anim_Text, speed);
+  } else {
+    anim_var = 0;
+    num++;
+  }
+}
 
 
 btn.addEventListener('click', function handleClick() {
-  if (num == (dialogs.length)) {
-    num = 0;
-  }
-  Label.textContent = dialogs[num];
-  num++;
+    
+    if (num == (dialogs.length))
+    {
+        num = 0;
+    }
+    Anim_Text();
+ 
 });
