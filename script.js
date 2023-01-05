091a05d20c7fac654411d2d30dd2b086a7d37440
Fit4a <67219161+nothome2005@users.noreply.github.com>
2023-01-05 22:04:29 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,10 @@
 const snd = new Audio("music/My-Feelings-Doki-Doki-Literature-Club.wav");
 const btn = document.getElementById('btn');
 const Label = document.getElementById('LABEL')
+const character = document.getElementById('character');
 snd.play();
 const dialogs = ['Привет!', 'Я наконец-то заработала!','Я так рада этому!']
+const dialog_img = ['girl','man','girl']
 var num = 0;
 var anim_var = 0;
 var speed = 50;
@@ -20,6 +22,12 @@ function Anim_Text() {
 
 
 btn.addEventListener('click', function handleClick() {
+    if (dialog_img[num] == 'man') {
+        character.src = 'dialog window/DialogWindow-H(2).png';
+    }
+    if (dialog_img[num] == 'girl') {
+        character.src = 'dialog window/DialogWindow-Komi(2).png'
+    }
     
     if (num == (dialogs.length))
     {
