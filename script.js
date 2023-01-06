7b7b962c3d6f9296954b09e89149738f4b44530e
Fit4a <67219161+nothome2005@users.noreply.github.com>
2023-01-06 19:38:25 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,17 +1,17 @@
-const snd = new Audio("music/My-Feelings-Doki-Doki-Literature-Club.wav");
 const keyboard_sound = new Audio('music/cf9b99726102246.wav');
+const snd = new Audio("music/My-Feelings-Doki-Doki-Literature-Club.wav");
 const btn = document.getElementById('btn');
-const Label = document.getElementById('LABEL')
+const Label = document.getElementById('LABEL');
 const character = document.getElementById('character');
 const slider = document.getElementById("myRange");
 const kbvm = document.getElementById("kb_volume_controller");
-
-
-const dialogs = ['Привет!', 'Я наконец-то заработала!','Я так рада этому!']
-const dialog_img = ['girl','man','girl']
+const style_file = document.getElementById("style_file");
+const dialogs = ['Привет!', 'Я наконец-то заработала!','Я так рада этому!'];
+const dialog_img = ['girl','man','girl'];
 var num = 0;
 var anim_var = 0;
 var speed = 50;
+snd.play();
 
 
 function change_dialog_logo() {
@@ -25,7 +25,7 @@ function change_dialog_logo() {
 
 function Anim_Text() {
 
-    change_dialog_logo()
+    change_dialog_logo();
     keyboard_sound.play();
     if (anim_var < (dialogs[num].length + 1)) {
         Label.textContent = dialogs[num].slice(0,anim_var);
@@ -40,7 +40,7 @@ function Anim_Text() {
 
 
 btn.addEventListener('click', function handleClick() {
-    change_dialog_logo()
+    change_dialog_logo();
     if (num == (dialogs.length))
     {
         num = 0;
@@ -55,4 +55,16 @@ slider.oninput = function() {
 kbvm.oninput = function() {
     keyboard_sound.volume = this.value/100;
 } 
-snd.play();
+
+
+window.onload = function() {
+    
+    if (screen.width == 2560) {
+        style_file.setAttribute('href',"style3.css")
+        console.log('OK');
+    }
+    if (screen.width == 1920) {
+        style_file.setAttribute('href',"style3_alternative.css")
+        console.log('NO');
+    }
+}
